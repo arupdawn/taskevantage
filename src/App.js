7123da4239d9da2eb957dc4640ce4fe07c752fb3
arupdawn <rupchandradawn@gmail.com>
2022-12-06 11:03:44 +0530

    completed the task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const baseURL = "https://randomuser.me/api";
+  const [users,setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+        const response = await axios({
+            method: 'get',
+            url: `https://randomuser.me/api`
+        });
+        console.log("response >>",response.data.results);
+        setUsers(response.data.results);
+        localStorage.setItem('userDetails',JSON.stringify(response.data.results));
+    }
+    getUserData();
+}, [])
+
+const fetchUser = async () => {
+      const response = await axios({
+        method: 'get',
+        url: `https://randomuser.me/api`
+      });
+      console.log("response >>",response.data.results);
+      setUsers(response.data.results);
+      localStorage.setItem('userDetails',JSON.stringify(response.data.results));
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users[0].name.title}{users[0].name.first}{users[0].name.last}<br/>
+      {users[0].email}<br/>
+      <button onClick={fetchUser}>Refresh</button>
     </div>
   );
 }
